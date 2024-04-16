0982b4ca9ab1a9dc104bbf1c762d6fa300752943
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2024-04-16 11:24:36 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include<Windows.h>
+#include<cstdint>
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg){
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリ終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//　標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+//windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!");
+
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = DrvGetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
+
+	//クライアント領域を元に実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	
+
+	return 0;
+}
