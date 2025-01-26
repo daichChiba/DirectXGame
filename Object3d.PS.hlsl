d2da3486cd892e74c7cf10ed095b39d2bdbc7d4b
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2025-01-26 19:17:29 +0900

    評価課題の内容をやったため、zipにまとめた。

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -30,14 +30,16 @@ PixelShaderOutput main(VertexShaderOutput input){
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     output.color = gMaterial.color * textureColor;
     if (gMaterial.enableLIghting != 0){
-        //Lightingする場合
+        //カメラの位置から頂点を引く
         float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
         float32_t3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
+        float RdatE = dot(reflectLight, toEye);
+        float specularPow = pow(saturate(RdatE), gMaterial.shininess); //反射強度
+        
         float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-        //output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
-        float RdotE = dot(reflectLight, toEye);
-        float specularPow = pow(saturate(RdotE), gMaterial.shininess); //反射強度
+        ////output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        
         //拡散反射
         float32_t3 diffuse =
         gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
@@ -46,7 +48,7 @@ PixelShaderOutput main(VertexShaderOutput input){
         gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
         //拡散反射+鏡面反射
         output.color.rgb = diffuse + specular;
-        //αは今まで通り
+        //アルファは今まで通り
         output.color.a = gMaterial.color.a * textureColor.a;
     }else{
         output.color = gMaterial.color * textureColor;
